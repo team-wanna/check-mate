6ffaaad218d06d6b2af97340fc6b7c7d575a6050
Wani <81760347+Wani93@users.noreply.github.com>
2021-09-27 23:53:22 +0900

    feat: Base Layout 화면 개발 (#20)

---
+++ frontend/src/components/templates/BaseLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <main>
+      <slot name="main"></slot>
+    </main>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseLayout'
+}
+</script>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  width: 1200px;
+  overflow: auto;
+}
+</style>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,28 +1,25 @@
-import Vue from 'vue';
-import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter);
-
-const routes: RouteConfig[] = [
+const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
 ];
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
