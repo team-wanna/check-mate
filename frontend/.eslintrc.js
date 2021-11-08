544de59801d51a8529c29623837e59e8bb97d4f9
Wani <devwani93@gmail.com>
2021-11-09 02:18:17 +0900

    etc: eslint rule 변경

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,16 +5,17 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-essential',
-    "eslint:recommended",
+    'eslint:recommended',
     '@vue/airbnb',
     '@vue/typescript/recommended',
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
