4bd04b798f33e3fe8dc42d090b554ecb0ca3002f
Wani <devwani93@gmail.com>
2021-12-09 00:01:58 +0900

    fix: 모달창 헤더에 chess 아이콘 제거

--- frontend/src/components/templates/BaseModal.vue
+++ frontend/src/components/templates/BaseModal.vue
@@ -3,9 +3,6 @@
     <div v-if="isShow" class="modal">
       <div class="container">
         <header class="base-modal-header">
-          <div class="logo-container">
-            <fa :icon="['fas', 'chess']" class="icon icon--large" />
-          </div>
           <fa
             :icon="['far', 'window-close']"
             class="icon icon--large"
@@ -69,14 +66,16 @@ export default defineComponent({
 .container {
   width: 800px;
   height: 550px;
-  border-radius: 5px;
+  border-radius: 20px;
   background-color: #ffffff;
 }
 .base-modal-header {
   display: flex;
-  justify-content: space-between;
+  justify-content: right;
+  //justify-content: space-between;
   align-items: center;
-  border-radius: 5px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
   background-color: #f8f9fa;
   height: 60px;
   padding: 0 15px;
